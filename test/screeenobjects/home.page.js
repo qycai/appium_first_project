0c8c5c9d302eb8dbc2b26f4fe1e75c9c9b6c4b9c
qycai <1071226064@qq.com>
2020-07-24 18:40:53 +0800

    [qunyao]add more content to TestAppTest.js

--- test/screeenobjects/home.page.js
+++ test/screeenobjects/home.page.js
@@ -15,11 +15,61 @@ class HomePage {
         return $('~Answer')
     }
 
+    get showAlertButton() {
+        const selector = `type == 'XCUIElementTypeButton' && name CONTAINS 'show alert'`;
+        return $(`-ios predicate string:${selector}`)
+    }
+
+    get alertMessage() {
+        const selector = `type=='XCUIElementTypeStaticText' && name CONTAINS 'this alert is so cool.'`;
+        return $(`-ios predicate string:${selector}`)
+    }
+
+    get cancelButton() {
+        return $('~Cancel')
+    }
+
+
+    get slider() {
+        const selector = `type=='XCUIElementTypeSlider' && name CONTAINS 'AppElem'`;
+        return $(`-ios predicate string:${selector}`)
+    }
+
+    get disabledButton() {
+        return $('~DisabledButton')
+    }
+
+    get checkCalendarButton() {
+        const selector = `type=='XCUIElementTypeButton' && name CONTAINS 'Check calendar authorized'`;
+        return $(`-ios predicate string:${selector}`)
+    }
+
+    get calendarMessage() {
+        return $('~not authorized')
+    }
+
     countSum(integerA, integerB) {
         this.integerA.setValue(integerA);
         this.integerB.setValue(integerB);
         this.sunButton.click();
     }
+
+    setSlider(sliderPercent) {
+        this.slider.setValue(sliderPercent)
+    }
+
+    showAlert() {
+        this.showAlertButton.click()
+    }
+
+    closeAlert() {
+        this.cancelButton.click()
+    }
+
+    checkCalendar() {
+        this.checkCalendarButton.click()
+    }
+
 }
 
 
--- test/specs/TestAppTest.js
+++ test/specs/TestAppTest.js
@@ -2,7 +2,26 @@ const homePage = require('../screeenobjects/home.page')
 
 describe('test TestApp', () => {
     it('should get correct answer from count sum function', function () {
-        homePage.countSum(1, 2)
-        expect(homePage.answer).toHaveText("3")
+        //check count sum
+        homePage.countSum(1, 2);
+        expect(homePage.answer).toHaveText("3");
+
+        //check show alert
+        homePage.showAlert();
+        expect(homePage.alertMessage).toHaveText("this alert is so cool.");
+        homePage.closeAlert();
+
+        //operate slider
+        homePage.setSlider(0.8);
+        driver.hideKeyboard();
+
+        //check disable button
+        expect(homePage.disabledButton).toBeDisabled()
+
+        //check calendar alert
+        homePage.checkCalendar();
+        expect(homePage.calendarMessage).toHaveText("not authorized")
+        homePage.closeAlert();
+
     });
 })
